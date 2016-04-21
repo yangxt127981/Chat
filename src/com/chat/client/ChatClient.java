9aee36ca8b6aff954d7c8efb0511fd9a65789ac0
Yang, Xiao-Tong <xiaotong.yang@hp.com>
2016-04-21 16:23:39 +0800

    chat0.6
    Change-Id: I0ca8689b2a3298345263c494abf98b5baca600be

--- src/com/chat/client/ChatClient.java
+++ src/com/chat/client/ChatClient.java
@@ -4,14 +4,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+import java.io.IOException;
+import java.net.*;
 public class ChatClient extends Frame{
     private TextField tf = new TextField();
     private TextArea ta = new TextArea();
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
        new ChatClient().lanuchFrame();
+       
 	}
 	
 	public void lanuchFrame(){
@@ -29,7 +30,20 @@ public class ChatClient extends Frame{
 		tf.addActionListener(new TfListener());
 		this.setVisible(true);
 		
+		connect();
 	}
+	
+	public void connect(){
+		try {
+			Socket s = new Socket("127.0.0.1",8888);
+			System.out.println("connect to server");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	private class TfListener implements ActionListener{
 
