041c3f99180aae37e10521bef0346f96456ae7ef
Yang, Xiao-Tong <xiaotong.yang@hp.com>
2016-04-26 17:18:36 +0800

    chat0.9
    Change-Id: I655ba672a2688e1c7060604e86d62337667bdcb3

--- src/com/chat/client/ChatServer.java
+++ src/com/chat/client/ChatServer.java
@@ -5,27 +5,47 @@ import java.net.*;
 
 public class ChatServer {
     private static boolean started = false;
+    static ServerSocket ss =null;
+    static Socket s = null;
+    static DataInputStream dis = null;
 	public static void main(String[] args) {
-       try {
-    	   ServerSocket ss = new ServerSocket(8888);
-    	   started = true;
-    	   while(started){
-		       boolean bconnected = false;
-    		   Socket s  = ss.accept();
-		       System.out.println("a client connected");
-		       bconnected = true;
-		       DataInputStream dis = new DataInputStream(s.getInputStream());
-		       while(bconnected){
-			       String str = dis.readUTF();
-			       System.out.println(str);
-
-		       }
-		    	dis.close();   
+    
+    	   try{
+    	       ss = new ServerSocket(8888);
+    	   }catch (IOException e){
+    		   e.printStackTrace();
     	   }
     	   
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    	   try{
+	    	   started = true;
+	    	   while(started){
+			       boolean bconnected = false;
+	    		   s  = ss.accept();
+			       System.out.println("a client connected");
+			       bconnected = true;
+			        dis = new DataInputStream(s.getInputStream());
+			       while(bconnected){
+				       String str = dis.readUTF();
+				       System.out.println(str);
+	
+			       }
+			    	dis.close();   
+    	      }
+			} catch (Exception e) {
+				
+				//e.printStackTrace();
+				System.out.println("client close");
+			}finally{
+				try{
+					if(dis!=null)
+						dis.close();
+					if(s!=null)
+					   s.close();
+					
+				}catch(IOException e1){
+					e1.printStackTrace();
+				}
+			}
 	}
 
 }
