3133083d72693b46626f6ea965aadf563577e3b1
Yang, Xiao-Tong <xiaotong.yang@hp.com>
2016-05-26 12:07:54 +0800

    chat thread

--- src/com/chat/client/ChatServer.java
+++ src/com/chat/client/ChatServer.java
@@ -5,51 +5,81 @@ import java.io.IOException;
 import java.net.*;
 
 public class ChatServer {
-   
-	public static void main(String[] args) {
-		    boolean started = false;
-		    ServerSocket ss =null;
-		    Socket s = null;
-		    DataInputStream dis = null;
-    	   try{
-    	       ss = new ServerSocket(8888);
-    	   }catch (BindException e){
-    		   	System.out.println("port is using.....");
-           }catch (IOException e){
-    		   e.printStackTrace();
-    	   }
-    	   
-    	   try{
-	    	   started = true;
-	    	   while(started){
-			       boolean bconnected = false;
-	    		   s  = ss.accept();
-			       System.out.println("a client connected");
-			       bconnected = true;
-			       dis = new DataInputStream(s.getInputStream());
-			       while(bconnected){
-				       String str = dis.readUTF();
-				       System.out.println(str);
-	
-			       }
-    	      }
-			} catch(EOFException e){
-				System.out.println("client closed!");
-			}catch (Exception e) {
-				
-				//e.printStackTrace();
-				System.out.println("client close");
-			}finally{
-				try{
-					if(dis!=null)
-						dis.close();
-					if(s!=null)
-					   s.close();
-					
-				}catch(IOException e1){
-					e1.printStackTrace();
-				}
+boolean started = false;
+ServerSocket ss = null;
+public static void main(String[] args) {
+	   new ChatServer().start();
+}
+public void start(){
+	   started = true;
+	   try{
+	       ss = new ServerSocket(8888);
+	   }catch (BindException e){
+		   	System.out.println("port is using.....");
+		   	System.exit(0);
+       }catch (IOException e){
+		   e.printStackTrace();
+	   }
+	   
+	   try{
+  	   while(started){
+  		   Socket s  = ss.accept();
+  		   Client c = new Client(s);
+  		   new Thread(c).start();
+		   System.out.println("a client connected");
+	      }
+		} catch(IOException e){
+          e.printStackTrace();
+       }finally{
+			try {
+				ss.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		}
+}
+	
+class Client implements Runnable{
+   private Socket s;
+   private DataInputStream dis =null;
+   private boolean bconnected = false;
+   
+   
+   public Client(Socket s){
+	   this.s = s;
+	   try {
+		dis = new DataInputStream(s.getInputStream());
+		bconnected = true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+   }
+	@Override
+   public void run() {
+		try {
+	       while(bconnected){
+				String str = dis.readUTF();
+			    System.out.println(str);
+				} 
+		}catch (EOFException e) {
+			System.out.println("client closed!");
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try{
+				if(dis!=null)
+					dis.close();
+				if(s!=null)
+				   s.close();
+				
+			}catch(IOException e1){
+				e1.printStackTrace();
+			}
+		} 
+		
 	}
-
+	
+}
 }
